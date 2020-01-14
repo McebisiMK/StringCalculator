e0bed7a16a1d353754bee9519cbfb273d1d175a6
mcebisim <mcebisim@sahomeloans.com>
2020-01-14 10:20:14 +0200

    Added functionality to exclude numbers greater than 1000

--- StringCalculator.Library/StringCalculator.cs
+++ StringCalculator.Library/StringCalculator.cs
@@ -1,20 +1,14 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using StringCalculator_Library.Exceptions;
 using StringCalculator_Library.IValidations;
-using StringCalculator_Library.Validations;
 
 namespace StringCalculator_Library
 {
-    public class StringCalculator
+    public class StringCalculator : IStringCalculator
     {
         private readonly IValidator _validator;
-
-        public StringCalculator() : this(new Validator())
-        {
-        }
 
         public StringCalculator(IValidator validator)
         {
@@ -23,20 +17,22 @@ namespace StringCalculator_Library
 
         public double Add(string input)
         {
-                if (string.IsNullOrWhiteSpace(input))
-                    throw new InvalidInputException();
+            if (string.IsNullOrWhiteSpace(input))
+                throw new InvalidInputException();
 
-                var stringNumbers = GetNumbers(input);  
-                var numbers = _validator.Validate(stringNumbers);
+            var stringNumbers = GetNumbers(input);
+            var numbers = _validator.Validate(stringNumbers);
 
-                return numbers.Sum();
+            return numbers
+                    .Where(number => number <= 1000)
+                    .Sum();
         }
 
         private IEnumerable<string> GetNumbers(string input)
         {
             var stringNumbers = Regex.Matches(input, @"-?[0-9]*\.?[0-9]+");
 
-            return stringNumbers.Cast<Match>().Select(match => match.Value) ;
+            return stringNumbers.Cast<Match>().Select(match => match.Value);
         }
-    } 
+    }
 }
